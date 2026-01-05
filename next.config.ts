7909d68c4468a25fc2fbde73c6c5438d9d1113f9
Ashwin Sevak <ashwinsevak2091@gmail.com>
2026-01-05 20:48:53 +0530

    feat: add `node-fetch` dependency, ignore new environment and script files, and allow Supabase images in `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "yyinahtqphrbareoyqfg.supabase.co",
+      },
+      {
+        protocol: "https",
         hostname: "*.cdninstagram.com",
       },
       {
